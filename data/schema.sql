1466374682dbd2bc4f6ddd3931d922bdcf2618d1
roxlukas <rox_lukas@wp.pl>
2014-11-14 08:28:18 +0100

    Update schema.sql
    Fix: pageContents in "lmpagecache" table can be larger than 64k, so I changed TEXT column to MEDIUMTEXT type

--- data/schema.sql
+++ data/schema.sql
@@ -1508,7 +1508,7 @@ CREATE TABLE IF NOT EXISTS `apiblueprints` (
 
 CREATE TABLE IF NOT EXISTS `lmpagecache` (
   `pageLabel` varchar(32) NOT NULL,
-  `pageContents` text NOT NULL,
+  `pageContents` mediumtext NOT NULL,
   `timestamp` TIMESTAMP NOT NULL,
   PRIMARY KEY (`pageLabel`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
