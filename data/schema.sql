89e0ddd9730ed1d1649bb909153f73d1f8316390
Lukasz Pozniak <rox_lukas@wp.pl>
2015-04-23 16:49:32 +0200

    bug fix
    Bug fix for lack of POS and inventory in LMeve despite having proper api
    key, also causes "POLLER HANGED" warnings.
    
    You can drop 'apistarbasedetail' table in LMeve database and then update
    it with new definition from schema.sql (this table should be near the
    bottom of schema.sql)

--- data/schema.sql
+++ data/schema.sql
@@ -1578,24 +1578,24 @@ CREATE TABLE IF NOT EXISTS `apipocolist` (
 -- schema delta for starbase detail, starbase fuel and crest industry system index
 
 CREATE TABLE IF NOT EXISTS `apistarbasedetail` (
-  `itemID` bigint(11) NOT NULL,
-  `state` int(11) NOT NULL,
-  `stateTimestamp` datetime NOT NULL,
-  `onlineTimestamp` datetime NOT NULL,
-  `usageFlags` int(11) NOT NULL,
-  `deployFlags` int(11) NOT NULL,
-  `allowCorporationMembers` int(11) NOT NULL,
-  `allowAllianceMembers` int(11) NOT NULL,
-  `useStandingsFrom` int(11) NOT NULL,
-  `onStandingDrop` int(11) NOT NULL,
-  `onStatusDropStanding` int(11) NOT NULL,
-  `onAggression` int(11) NOT NULL,
-  `onCorporationWar` int(11) NOT NULL,
-  `corporationID` bigint(11) NOT NULL,
-  PRIMARY KEY (`itemID`),
-  KEY `corporationID` (`corporationID`)
+`itemID` bigint(11) NOT NULL,
+`state` int(11) NOT NULL,
+`stateTimestamp` datetime NOT NULL,
+`onlineTimestamp` datetime NOT NULL,
+`usageFlags` int(11) NOT NULL,
+`deployFlags` int(11) NOT NULL,
+`allowCorporationMembers` int(11) NOT NULL,
+`allowAllianceMembers` int(11) NOT NULL,
+`useStandingsFrom` int(11) NOT NULL,
+`onStandingDrop` int(11) NOT NULL,
+`onStatusDrop` int(11) NOT NULL,
+`onStatusDropStanding` int(11) NOT NULL,
+`onAggression` int(11) NOT NULL,
+`onCorporationWar` int(11) NOT NULL,
+`corporationID` bigint(11) NOT NULL,
+PRIMARY KEY (`itemID`),
+KEY `corporationID` (`corporationID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE IF NOT EXISTS `apistarbasefuel` (
   `itemID` bigint(11) NOT NULL,
