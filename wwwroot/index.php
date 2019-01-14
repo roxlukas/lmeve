65a0b71ee73ebd8c771ed2f9a9d04caba6d1dc11
Lukasz Pozniak <pozniak.lukasz@gmail.com>
2019-01-14 13:12:25 +0100

    checking if mbstring is enabled in PHP - will tell user to check configuration if mbstring extension is not enabled

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -32,6 +32,8 @@
 **********************************************************************************/
 header('Content-Type: text/html; charset=UTF-8');
 
+if (!function_exists('mb_internal_encoding')) die('Please make sure you\'ve installed and enabled php-mbstring');
+
 mb_internal_encoding('UTF-8'); 
 mb_http_output('UTF-8'); 
 mb_http_input('UTF-8'); 
